b27bebe01c5281d08f0cda9b973366d4bb0f3566
Pablo <elpablos79@gmail.com>
2019-03-09 16:46:55 +0100

    Push

---
+++ cardRpg/src/main/java/myownGame/cardRpg/CardRpgApplication.java
@@ -0,0 +1,13 @@
+package myownGame.cardRpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardRpgApplication.class, args);
+	}
+
+}
