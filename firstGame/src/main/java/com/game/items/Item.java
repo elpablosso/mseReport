8cb579ab411c04da2be194341a6393822d632333
Pablo <elpablos79@gmail.com>
2019-03-10 15:42:37 +0100

    Push 1

--- firstGame/src/main/java/com/game/items/Item.java
+++ firstGame/src/main/java/com/game/items/Item.java
@@ -7,4 +7,26 @@ public abstract class Item {
     private final String itemDescription;
     private final int itemValue;
 
+    public Item(int itemId, String itemName, String itemDescription, int itemValue) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.itemDescription = itemDescription;
+        this.itemValue = itemValue;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public int getItemValue() {
+        return itemValue;
+    }
 }
