638f6f3662bf568e636fce4b2a9fcbe0a6caabea
Pablo <elpablos79@gmail.com>
2019-03-10 15:39:57 +0100

    Push 1

--- firstGame/src/main/java/com/game/items/Item.java
+++ firstGame/src/main/java/com/game/items/Item.java
@@ -3,5 +3,8 @@ package com.game.items;
 public abstract class Item {
 
     private final int itemId;
+    private final String itemName;
+    private final String itemDescription;
+    private final int itemValue;
 
 }
