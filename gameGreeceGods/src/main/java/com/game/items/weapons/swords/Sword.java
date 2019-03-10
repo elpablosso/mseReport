5551efce28ad124fccf9e050f544ac17c2eb4e44
Pablo <elpablos79@gmail.com>
2019-03-10 14:50:41 +0100

    Test

---
+++ gameGreeceGods/src/main/java/com/game/items/weapons/swords/Sword.java
@@ -0,0 +1,10 @@
+package com.game.items.weapons;
+
+import com.game.items.Weapon;
+import com.game.items.enums.WeaponType;
+
+public class Sword extends Weapon {
+
+    private final WeaponType weaponType = WeaponType.SWORD;
+
+}
