4175076f8117825d021b4dcd8016415ac2c8097c
Pablo <elpablos79@gmail.com>
2019-03-28 21:01:52 +0100

    Rebuild

---
+++ ReportingTool/src/main/java/com/maven/pablo/reportingtool/ReportingtoolApplication.java
@@ -0,0 +1,16 @@
+package com.maven.pablo.reportingtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportingtoolApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportingtoolApplication.class, args);
+	}
+
+
+
+}
---
+++ ReportingTool/src/test/java/com/maven/pablo/reportingtool/ReportingtoolApplicationTests.java
@@ -0,0 +1,16 @@
+package com.maven.pablo.reportingtool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReportingtoolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
